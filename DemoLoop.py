6238e5fe71e9bc798a4e911f76250a70473c2d8c
papasmf1 <papasmf1@gmail.com>
2021-06-30 11:29:44 +0900

    반복구문

--- DemoLoop.py
+++ DemoLoop.py
@@ -32,3 +32,35 @@ for i in lst:
     if i % 2 == 0:
         continue 
     print("Item:{0}".format(i))
+
+#수열함수 
+print( list(range(10)) )
+print( list(range(1,11)) )
+print( list(range(10,0,-1)) )
+#수동으로 반복을 5번 
+for i in range(5):
+    print(i)
+
+#리스트 컴프리헨션(함축, 압축)
+lst = [1,2,3,4,5,6,7,8,9,10]
+#전체 리스트에서 필터리을 하고 가공 
+print( [i**2 for i in lst if i > 5] )
+
+d = {100:"apple", 200:"kiwi", 300:"orange"}
+print( [v.upper() for v in d.values()] )
+
+#필터링하는 함수
+lst = [10, 25, 30]
+iterL = filter(None, lst)
+for i in iterL:
+    print("Item:{0}".format(i))
+
+print("---필터링---")
+#함수를 정의(4장)
+def getBiggerThan20(i):
+    return i > 20
+
+iterL = filter(getBiggerThan20, lst)
+for i in iterL:
+    print("Item:{0}".format(i))
+
