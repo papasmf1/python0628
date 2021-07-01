2520c0c53ec30d51fc26faf83995474e77bc085a
papasmf1 <papasmf1@gmail.com>
2021-07-01 15:22:21 +0900

    웹크롤링2

--- web1.py
+++ web1.py
@@ -10,4 +10,22 @@ page = open("c:/work/test01.html", "rt", encoding="utf-8").read()
 #검색이 용이한 객체(인터넷상에 있는 html문서를 검색)
 soup = BeautifulSoup(page, "html.parser")
 #멋지게 보여달라~~
-print( soup.prettify() )
+#print( soup.prettify() )
+
+#<p>태그를 모두 가져오기 ==> 리스트 형식으로 리턴
+#print( soup.find_all("p") )
+
+#<a>태그 
+#print( soup.find_all("a") )
+
+#첫번째 <p>만 가져오기 
+#print( soup.find("p") )
+
+#조건이 있는 경우: <p class="outer-text">
+#파이썬 언어에서 class는 타입을 정의하는 구문
+#print( soup.find_all("p", class_="outer-text") )
+
+#태그 내부에 컨텐츠만 검색(text속성을 사용) 
+for tag in soup.find_all("p"):
+    print( tag.text.replace("\n", "").strip() )
+
